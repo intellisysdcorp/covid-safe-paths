3eebdaa8901fe3293464b68d5a7fd05ad42eed76
David De La Rosa <davidrr261096@hotmail.com>
2020-06-01 16:18:51 -0400

    feedbacks

--- app/helpers/imagesSource.js
+++ app/helpers/imagesSource.js
@@ -1,7 +1,31 @@
+/*
+ Get Image Crop base on array
+ 
+ @params img {Object} image crop object
+ @params crops {Array} crops name order by the first name exist on image crop
+@returns {String} the image url that match with the first crop name found in the image object
+*/
+const cropsName = [
+  '120x67',
+  '120x80',
+  '120x86',
+  '250x141',
+  '250x167',
+  '250x179',
+];
+const getImageCrop = (img, crops) => {
+  const firstCropFound = crops.find(cropName => img[cropName]);
+  return firstCropFound ? img[firstCropFound] : img.master;
+};
+
 export default function(dataList) {
   const data = dataList.news.map(item => ({
     ...item,
-    img: { source: { uri: Object.values(item.img)[3] } },
+    img: {
+      source: {
+        uri: getImageCrop(item.img, cropsName),
+      },
+    },
   }));
 
   return data;
