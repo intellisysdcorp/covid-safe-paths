65f3c64affc38418c88d004f4bd3e6bf87e4c3db
David De La Rosa <davidrr261096@hotmail.com>
2020-06-08 12:55:43 -0400

    using the same instance to change languages

--- app/views/DR/Maps/index.js
+++ app/views/DR/Maps/index.js
@@ -1,13 +1,13 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import React from 'react';
+import { withTranslation } from 'react-i18next';
 import { SafeAreaView, StyleSheet } from 'react-native';
 
-import languages from '../../../locales/languages';
 import Map from './Map';
 
 const Tab = createMaterialTopTabNavigator();
 
-const index = () => {
+const index = ({ t }) => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <Tab.Navigator
@@ -20,14 +20,14 @@ const index = () => {
         <Tab.Screen
           name='Hospitals'
           options={{
-            tabBarLabel: languages.t('navigation.hospitals_maps'),
+            tabBarLabel: t('navigation.hospitals_maps'),
           }}
           component={Map}
         />
         <Tab.Screen
           name='Laboratories'
           options={{
-            tabBarLabel: languages.t('navigation.laboratories_maps'),
+            tabBarLabel: t('navigation.laboratories_maps'),
           }}
           component={Map}
         />
@@ -45,4 +45,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default index;
+export default withTranslation()(index);
