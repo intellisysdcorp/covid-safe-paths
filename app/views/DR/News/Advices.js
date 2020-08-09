680067810879e1b551af5122fc7b832ba2d1fffb
David De La Rosa <davidrr261096@hotmail.com>
2020-08-09 15:43:55 -0400

    getting advice data from service

--- app/views/DR/News/Advices.js
+++ app/views/DR/News/Advices.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 
-import iconAdvertisement from '../../../assets/images/idea.jpg';
 import imgAdvertisement from '../../../assets/images/recommendations.jpg';
 import HeaderImage from '../../../components/DR/HeaderImage';
 import List from '../../../components/DR/List';
 import Colors from '../../../constants/colors-dr';
-import data from '../../../constants/DR/RecommendationData';
+import { FIREBASE_SERVICE } from '../../../constants/DR/baseUrls';
 import languages from '../../../locales/languages';
 
+const ADVICE_URL = `${FIREBASE_SERVICE}/advices`;
 export default function Advices({ navigation }) {
-  const recommendationData = data.map(item => ({
-    ...item,
-    icon: { iconName: 'comment-medical', color: Colors.PINK },
-  }));
+  const [advices, setAdvices] = useState([]);
+
+  const addImgToAdvice = recommendationList =>
+    recommendationList.map(item => ({
+      ...item,
+      icon: { iconName: 'comment-medical', color: Colors.PINK },
+    }));
+
+  useEffect(() => {
+    fetch(ADVICE_URL)
+      .then(({ data }) => {
+        const bulletinsData = addImgToAdvice(data);
+        setAdvices(bulletinsData);
+      })
+      .catch(() => {
+        setAdvices([]);
+      });
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -23,7 +37,7 @@ export default function Advices({ navigation }) {
       />
       <ScrollView>
         <List
-          data={recommendationData}
+          data={advices}
           titleLinesNum={2}
           navigation={navigation}
           switchScreenTo='PDFView'
