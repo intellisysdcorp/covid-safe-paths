89c1ec18ba71ed49a7aa31a752a6aed83ddc7667
David De La Rosa <davidrr261096@hotmail.com>
2020-09-08 16:30:45 -0400

    working on firebase analitycs

--- android/app/src/main/java/com/optic/covdr/MainApplication.java
+++ android/app/src/main/java/com/optic/covdr/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 import com.reactnativecommunity.androidprogressbar.RNCProgressBarPackage;
 import com.github.reactnativecommunity.location.RNLocationPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
