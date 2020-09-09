7e5055e4629ae52d14366adce66b488bd3545abf
Rut Santos <rsantos@intellisys.com.do>
2020-09-09 10:28:24 -0400

    Adding missing dependecies needed for react-native-pdf to work correctly

--- android/app/src/main/java/com/optic/covdr/MainApplication.java
+++ android/app/src/main/java/com/optic/covdr/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.androidprogressbar.RNCProgressBarPackage;
-import com.github.reactnativecommunity.location.RNLocationPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.marianhello.bgloc.BackgroundGeolocationFacade;
 import com.facebook.react.ReactNativeHost;
