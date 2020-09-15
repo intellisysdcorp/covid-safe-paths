b905c46db7fe329edeba464396ddc130038ec3d3
Rut Santos <rsantos@intellisys.com.do>
2020-09-15 12:49:12 -0400

    Fixing problems with the test and Detox

--- e2e/helpers/onboarding.js
+++ e2e/helpers/onboarding.js
@@ -1,7 +1,6 @@
 import FinishSetup from '../pages/FinishSetup.po.js';
 import Onboarding1 from '../pages/Onboarding1.po.js';
 import Onboarding2 from '../pages/Onboarding2.po.js';
-import Onboarding3 from '../pages/Onboarding3.po.js';
 import Onboarding4 from '../pages/Onboarding4.po.js';
 import SignEula from '../pages/SignEula.po.js';
 
@@ -14,9 +13,6 @@ export const navigateThroughPermissions = async languageStrings => {
 
   await Onboarding2.isOnScreen(languageStrings);
   await Onboarding2.tapButton(languageStrings);
-
-  await Onboarding3.isOnScreen(languageStrings);
-  await Onboarding3.tapButton(languageStrings);
 
   await Onboarding4.isOnScreen(languageStrings);
   await Onboarding4.tapButton(languageStrings);
