68aed47b589815afe1546c7ac1c1f40bb78135bd
David De La Rosa <davidrr261096@hotmail.com>
2020-09-17 16:41:51 -0400

    adding validate certification to report and contact request

--- app/helpers/DR/validateResponse.js
+++ app/helpers/DR/validateResponse.js
@@ -1,6 +1,40 @@
+import axios from 'axios';
+import { Alert } from 'react-native';
+
+import { FIREBASE_SERVICE } from '../../constants/DR/baseUrls';
 import getToken from '../../services/DR/getToken';
 
-export default async function(url, method, body) {
+export async function validateCertificate(
+  t,
+  url,
+  request,
+  method = '',
+  body = {},
+) {
+  const domainUrl = url.slice(8, url.indexOf('/', 9)).trim();
+  const VALIDATE_CERTIFICATE_URL = `${FIREBASE_SERVICE}/validate/ssl-certificate`;
+
+  const data = await axios.post(VALIDATE_CERTIFICATE_URL, { domainUrl });
+
+  if (data.status === 200) {
+    return new Promise(resolve =>
+      Alert.alert(t('label.attention'), t('label.attentionMessage'), [
+        {
+          text: t('report.no'),
+          style: 'cancel',
+          onPress: () => resolve(false),
+        },
+        {
+          text: t('report.yes'),
+          onPress: () =>
+            resolve(request ? validateResponse(url, method, body) : true),
+        },
+      ]),
+    );
+  }
+}
+
+export async function validateResponse(url, method, body) {
   const responseFunc = (body, token) => {
     return fetch(url, {
       method,
