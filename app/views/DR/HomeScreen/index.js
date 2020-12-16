cea9ba666a1e0a5e30010393e2ab761cf5642aff
David De La Rosa <davidrr261096@hotmail.com>
2020-12-15 21:29:58 -0400

    deleting and comment previous implementation

--- app/views/DR/HomeScreen/index.js
+++ app/views/DR/HomeScreen/index.js
@@ -20,32 +20,21 @@ import {
 } from '../../../components/DR/ActionCards/ActionCards.js';
 import CasesStatistics from '../../../components/DR/CasesStatistics';
 import Colors from '../../../constants/colors';
-import { FIREBASE_SERVICE } from '../../../constants/DR/baseUrls';
-import fetch from '../../../helpers/Fetch';
-import {
-  GetStoreData,
-  RemoveStoreData,
-  SetStoreData,
-  saveUserState,
-} from '../../../helpers/General';
+import { GetStoreData, SetStoreData } from '../../../helpers/General';
 import {
   StateEnum,
   StateIcon,
   checkCurrentState,
   getMainText,
 } from '../../../helpers/LocationHelpers';
-import DialogAdvice from '../../DialogAdvices';
+// import DialogAdvice from '../../DialogAdvices';
 import styles from './style';
 
 class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      notified: false,
-      useType: '',
       refreshing: false,
-      covidId: '',
-      covidUserNickname: '',
       statusVisible: true,
       currentState: StateEnum.NO_CONTACT,
     };
@@ -61,44 +50,21 @@ class HomeScreen extends Component {
     this.setState({ currentState: newState });
   }
 
-  filterState = async (userList, userCovidId) => {
-    const filterUserList = userList.filter(user => user.covidId !== undefined);
-    const userListConvert = filterUserList.map(user =>
-      user.covidId === userCovidId ? { ...user, positive: true } : user,
-    );
-
-    await SetStoreData('users', userListConvert);
-  };
-
-  setAndGetUserState = userList => {
-    const promiseList = userList.map(userState => {
-      return fetch(`${FIREBASE_SERVICE}/covid-state/${userState.covidId}`);
-    });
-
-    Promise.all(promiseList).then(state => {
-      const negativeUserToPositive = state.find(
-        ({ data }) => data.positive === true && data.haveBeenNotified === false,
-      );
-
-      if (negativeUserToPositive) {
-        const {
-          data: { covidId },
-        } = negativeUserToPositive;
-
-        this.filterState(userList, covidId);
-        const { use, name = 'Usted' } = userList.find(
-          user => user.covidId === covidId,
-        );
-
-        saveUserState({ covidId, positive: true, haveBeenNotified: true });
-        this.setState({
-          notified: true,
-          useType: use,
-          covidUserNickname: name,
-          covidId,
-        });
-      }
-    });
+  validateUserState = userList => {
+    console.log(userList);
+    // if (covidUserNickname === 'Usted') {
+    //   this.props.navigation.navigate('PositiveOnboarding', {
+    //     positive: true,
+    //     use: useType,
+    //     covidId,
+    //   });
+    // } else {
+    //   this.props.navigation.navigate('EpidemiologicResponse', {
+    //     screen: 'EpidemiologicReport',
+    //     params: { nickname: covidUserNickname, path: false },
+    //     showDialog: useType === 'mySelf' ? true : false,
+    //   });
+    // }
   };
 
   refresh = () => {
@@ -109,33 +75,16 @@ class HomeScreen extends Component {
     this.setState({ refreshing: false });
   };
 
-  handlerPositiveState = () => {
-    const { useType, covidId, covidUserNickname } = this.state;
-    this.setState({ notified: false });
-
-    if (covidUserNickname === 'Usted') {
-      this.props.navigation.navigate('PositiveOnboarding', {
-        positive: true,
-        use: useType,
-        covidId,
-      });
-    } else {
-      this.props.navigation.navigate('EpidemiologicResponse', {
-        screen: 'EpidemiologicReport',
-        params: { nickname: covidUserNickname, path: false },
-        showDialog: useType === 'mySelf' ? true : false,
-      });
-    }
-  };
-
   async componentDidMount() {
     setTimeout(() => {
       this.setState({ statusVisible: false });
     }, 7000);
 
     const locationState = await GetStoreData('locationState', false);
-    const covidIdList = await GetStoreData('covidIdList', false);
-    let userList = await GetStoreData('users', false);
+    // const userList = await GetStoreData('users', false);
+    // if (userList) {
+    //   this.validateUserState(userList);
+    // }
 
     if (!locationState) {
       try {
@@ -145,16 +94,6 @@ class HomeScreen extends Component {
         await SetStoreData('locationState', true);
       } catch (error) {
         console.log('Something went wrong with analytics ', error);
-      }
-    }
-
-    if (userList) {
-      if (covidIdList) {
-        userList = userList.concat(covidIdList);
-        RemoveStoreData(covidIdList);
-        this.setAndGetUserState(userList);
-      } else {
-        this.setAndGetUserState(userList);
       }
     }
   }
@@ -174,7 +113,7 @@ class HomeScreen extends Component {
   render() {
     const {
       props: { t, navigation },
-      state: { refreshing, notified, covidUserNickname, statusVisible },
+      state: { refreshing, statusVisible },
     } = this;
     return (
       <SafeAreaView style={{ flex: 1 }}>
@@ -226,11 +165,11 @@ class HomeScreen extends Component {
             </View>
           )}
         </View>
-        <DialogAdvice
+        {/* <DialogAdvice
           visible={notified}
           text={`${covidUserNickname} ${t('label.positive_covid_message')}`}
           close={this.handlerPositiveState}
-        />
+        /> */}
       </SafeAreaView>
     );
   }
