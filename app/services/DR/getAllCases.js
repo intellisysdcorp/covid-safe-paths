408579b4a0cf559023b08e4f5b070b8a27fe7f9d
Fausto Jaquez <Faustojr03@hotmail.com>
2020-12-22 14:32:23 -0400

    Fixing dashboard data not showing when there are not any data with the date of today

--- app/services/DR/getAllCases.js
+++ app/services/DR/getAllCases.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 import { MEPID_URL_API } from '../../constants/DR/baseUrls';
 import fetch from '../../helpers/Fetch';
 import { getTokenMepid } from './getToken';
@@ -5,17 +7,24 @@ import { getTokenMepid } from './getToken';
 export async function getAllCases(date) {
   let savedToken = await getTokenMepid();
 
-  const responseFunc = token =>
+  const responseFunc = (date, token) =>
     fetch(`${MEPID_URL_API}/api/boletin/${date}`, {
       headers: { Authorization: `Bearer ${token}` },
     });
 
   try {
-    let response = await responseFunc(savedToken);
+    let response = await responseFunc(date, savedToken);
 
     if (response.data.error) {
       savedToken = await getTokenMepid(true);
-      response = await responseFunc(savedToken);
+      response = await responseFunc(date, savedToken);
+    }
+
+    if (!response.data[0].casos_acumulados) {
+      const dateBefore = moment(date)
+        .subtract(1, 'day')
+        .format('YYYY-MM-DD');
+      response = await responseFunc(dateBefore, savedToken);
     }
 
     return response.data;
