4e7ca48a649f718efb8ca0ab15aacfa097bf058c
Fausto Jáquez <46538610+Faustrox@users.noreply.github.com>
2020-12-23 08:58:06 -0400

    Fixing problem with latest data available on dashboard (#202)
    * Fixing dashboard data not showing when there are not any data with the date of today
    
    * Fixing date on calendar
    
    * Solving feedbacks
    
    * Deleting unnecesary code

--- app/services/DR/getAllCases.js
+++ app/services/DR/getAllCases.js
@@ -5,17 +5,17 @@ import { getTokenMepid } from './getToken';
 export async function getAllCases(date) {
   let savedToken = await getTokenMepid();
 
-  const responseFunc = token =>
+  const responseFunc = (date, token) =>
     fetch(`${MEPID_URL_API}/api/boletin/${date}`, {
       headers: { Authorization: `Bearer ${token}` },
     });
 
   try {
-    let response = await responseFunc(savedToken);
+    let response = await responseFunc(date, savedToken);
 
     if (response.data.error) {
       savedToken = await getTokenMepid(true);
-      response = await responseFunc(savedToken);
+      response = await responseFunc(date, savedToken);
     }
 
     return response.data;
